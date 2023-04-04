6372b9ef870ec8aa304fc394e2995f01b4516ca3
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-04-04 15:28:25 +0200

    Add histogram plotter: refacto

--- to_histogram.py
+++ to_histogram.py
@@ -6,13 +6,11 @@ from collections import defaultdict
 import argparse
 import os
 
-def create_histogram(csv_file, output_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def read_csv_data(csv_file):
+    data = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
     with open(csv_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        data = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
         for row in reader:
             if row['project_name'] != 'all':
@@ -23,34 +21,33 @@ def create_histogram(csv_file, output_dir):
                 data[project_name][username]['single'] += single_review_count
                 data[project_name][username]['multiple'] += multiple_review_count
 
+    return data
+
+
+def create_review_plot(project_name, project_data, review_type, output_dir):
+    sorted_users = sorted(project_data.keys(), key=lambda user: project_data[user]['single'] + project_data[user]['multiple'], reverse=True)
+
+    fig, ax = plt.subplots()
+    categories = [username for username in sorted_users]
+    counts = [project_data[user][review_type] for user in sorted_users]
+    ax.bar(categories, counts)
+    ax.set_ylabel(f'Number of {review_type.capitalize()} Reviews')
+    ax.set_title(f'{review_type.capitalize()} Review Counts for Project: {project_name}')
+    plt.xticks(rotation='vertical')
+    fig.tight_layout()
+    fig.savefig(os.path.join(output_dir, f'{project_name}_{review_type}_review_histogram.png'))
+    plt.close(fig)
+
+
+def create_histogram(csv_file, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    data = read_csv_data(csv_file)
+
     for project_name, project_data in data.items():
-        # Sort users by total number of MRs in descending order
-        sorted_users = sorted(project_data.keys(), key=lambda user: project_data[user]['single'] + project_data[user]['multiple'], reverse=True)
-
-        # Single review plot
-        fig_single, ax_single = plt.subplots()
-        categories_single = [username for username in sorted_users]
-        counts_single = [project_data[user]['single'] for user in sorted_users]
-        ax_single.bar(categories_single, counts_single)
-        ax_single.set_ylabel('Number of Single Reviews')
-        ax_single.set_title(f'Single Review Counts for Project: {project_name}')
-        plt.xticks(rotation='vertical')  # Rotate x-axis labels (usernames) vertically
-        fig_single.tight_layout()  # Adjust layout to prevent label overlap
-        fig_single.savefig(os.path.join(output_dir, f'{project_name}_single_review_histogram.png'))
-        plt.close(fig_single)  # Close the single review figure after saving it
-
-        # Multiple review plot
-        fig_multiple, ax_multiple = plt.subplots()
-        categories_multiple = [username for username in sorted_users]
-        counts_multiple = [project_data[user]['multiple'] for user in sorted_users]
-        ax_multiple.bar(categories_multiple, counts_multiple)
-        ax_multiple.set_ylabel('Number of Multiple Reviews')
-        ax_multiple.set_title(f'Multiple Review Counts for Project: {project_name}')
-        plt.xticks(rotation='vertical')  # Rotate x-axis labels (usernames) vertically
-        fig_multiple.tight_layout()  # Adjust layout to prevent label overlap
-        fig_multiple.savefig(os.path.join(output_dir, f'{project_name}_multiple_review_histogram.png'))
-        plt.close(fig_multiple)  # Close the multiple review figure after saving it
-
+        create_review_plot(project_name, project_data, 'single', output_dir)
+        create_review_plot(project_name, project_data, 'multiple', output_dir)
 
 def main():
     parser = argparse.ArgumentParser(description='Create histograms from review counts CSV.')
