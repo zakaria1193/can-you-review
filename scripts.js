53c17acc98b0fe8cc09c545422286a306f282dc8
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-04-04 16:40:38 +0200

    scripts.js: Selector defaults to All

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
+
 const projectSelector = document.getElementById('project-selector');
 const singleReviewImage = document.getElementById('single-review-image');
 const multipleReviewImage = document.getElementById('multiple-review-image');
 
 function updateProjectList(projects) {
+    projects.sort(); // Sort project names alphabetically
+
+    // Remove "all" from project names if it exists
+    const allIndex = projects.indexOf('all');
+    if (allIndex !== -1) {
+        projects.splice(allIndex, 1);
+    }
+
+    // Add the "All" option as the default selection
+    const allOption = document.createElement('option');
+    allOption.value = 'all';
+    allOption.textContent = 'All projects';
+    projectSelector.appendChild(allOption);
+
+    // Add the sorted project names as options
     projects.forEach((project) => {
         const option = document.createElement('option');
         option.value = project;
@@ -25,9 +41,13 @@ function getProjectNames() {
 
 function updateProject() {
     const project = projectSelector.value;
-    singleReviewImage.src = `./output/${project}_single_review_histogram.png`;
-    multipleReviewImage.src = `./output/${project}_multiple_review_histogram.png`;
+    if (project === 'All') {
+        singleReviewImage.src = './output/All_single_review_histogram.png';
+        multipleReviewImage.src = './output/All_multiple_review_histogram.png';
+    } else {
+        singleReviewImage.src = `./output/${project}_single_review_histogram.png`;
+        multipleReviewImage.src = `./output/${project}_multiple_review_histogram.png`;
+    }
 }
 
 getProjectNames(); // Fetch project names and populate the selector
-
